fe908d34054969b4cb38cc8dfa5695073c7c1763
SebastianGiordano <sebagr3322@gmail.com>
2023-06-08 18:20:49 -0400

    carrito basico

--- js/app.js
+++ js/app.js
@@ -4,33 +4,45 @@ const AGREGAR = 1;
 const VACIAR = 2;
 const VERTOTAL = 3;
 
+
 let precio = 0;
 let cantidad = 0;
+let precioFinal = 0;
 let total = 0;
 let opciones = '';
 let continuar = '';
 
 
+function borrar(precio, cantidad, total){
+    total = total - precio*cantidad;
+    return total
+}
+
 alert('Bienvenido a nuestra tienda!')
+
 do{
-    opciones = Number(prompt('¿Que te gustaria hacer? \n 1-Agregar un producto \n 2-Vaciar el carrito \n 3-Ver el total'))
+opciones = Number(prompt('QUE OPERACION TE GUSTARIA REALIZAR?\n 1- AGREGAR UN PRODUCTO \n 2- BORRAR UN PRODUCTO \n 3- VER TOTAL'))
+switch (opciones) {
+case AGREGAR:
+    precio = +prompt("ingrese el precio del producto");
+    cantidad = +prompt("Cuantos desea agregar?")
+    total= total + precio*cantidad
+    console.log("AGREGANDO PRODUCTO(S)...")
+    break
+case VACIAR:
+    precio = +prompt("indique su precio")
+    cantidad= +prompt("cuantos va a eliminar?")
+    total= total - precio*cantidad;
+    console.log("BORRANDO ITEM(S)...")
+    break
+case VERTOTAL:
+alert("El total de tu compra es: $" + total)
+default:
+    total = 0;
+}
 
 
-    switch(opciones){
-        case AGREGAR:
-        precio = Number(prompt('Ingresa el valor del producto'))
-        cantidad = Number(prompt('¿Cuantos deseas agregar?'))
-        console.log('agregando producto...')
-        alert('El valor de tu producto es: ' + precio*cantidad)
-        break
-        case VACIAR:
-        total = 0
-        console.log('vaciando el carrito...')
-        alert('tu carrito fue vaciado correctamente')
-        break
-        case VERTOTAL:
-    }
-
-    continuar = prompt('¿Te gustaria realizar otra accion? si/no')
-}while (continuar == 'si')
+console.log("Gracias por comprar con nosotros!")
+continuar = prompt("Te gustaria realizar otra operacion?")
+}while(continuar == "si")
 
